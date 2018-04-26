e57e7aa10ba4b83f5ea82b2dc71b9a491ef5aed0
Derry Santoso <derrysantoso7@gmail.com>
2018-04-26 20:06:01 +0700

    Set DB and migration

--- database/migrations/2018_04_26_125912_create_ratings_table.php
+++ database/migrations/2018_04_26_125912_create_ratings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateItemsTable extends Migration
+class CreateRatingsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateItemsTable extends Migration
      */
     public function up()
     {
-        Schema::create('items', function (Blueprint $table) {
+        Schema::create('ratings', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('item_mat_id');
+            $table->string('user_id');
             $table->string('item_id');
-            $table->string('title');
+            $table->string('rating');
             $table->timestamps();
         });
     }
@@ -29,6 +29,6 @@ class CreateItemsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('items');
+        Schema::dropIfExists('ratings');
     }
 }
